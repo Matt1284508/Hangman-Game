6ea81035249b74f35bc3fc535bd59a52b0120997
Matt Millsaps <mattmillsaps.dev@gmail.com>
2022-11-01 21:35:12 -0400

    loop  user prompt until all '_' are replaced

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import random
 ###  WORD LIBRARY  ###
 word_list = ["aardvark", "baboon", "camel"]
 
+end_of_game = False
+
 ### GENERATE A RANDOM WORD ###
 chosen_word = random.choice(word_list)
 #### DEBUG ####
@@ -13,14 +15,20 @@ display = []
 for letter in chosen_word:
     display.append("_")
 
+while not end_of_game:
+    
 ### ASK USER TO GUESS LETTER ###
-guess = input("Please Guess a letter: ").lower()
+    guess = input("Please Guess a letter: ").lower()
 
 ### CHECK IF guess IS IN chosen_word ###
-for position in range(len(chosen_word)):
-    letter = chosen_word[position]
-    if letter == guess:
-        display[position] = letter
-
+    for position in range(len(chosen_word)):
+     letter = chosen_word[position]
+     if letter == guess:
+         display[position] = letter
+         
+    print(display)
+    
+    if "_" not in display: 
+        end_of_game = True
         
-print(display)
+print('You Win!')
