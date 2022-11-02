92f0fdf1d8af01693100392379869ec6a901b915
Matt Millsaps <mattmillsaps.dev@gmail.com>
2022-11-02 19:02:41 -0400

    added ASCII art print functions

--- main.py
+++ main.py
@@ -18,25 +18,29 @@ for letter in chosen_word:
     display.append("_")
 
 while not end_of_game:
-    
 ### ASK USER TO GUESS LETTER ###
-    guess = input("Please Guess a letter: ").lower()
-
+    guess = input("\nPlease Guess a letter: ").lower()
+    
 ### CHECK IF guess IS IN chosen_word ###
     for position in range(len(chosen_word)):
         letter = chosen_word[position]
         if letter == guess:
             display[position] = letter
+    print(hangman_ASCII.stages[user_life_counter])
+
+            
     if guess not in chosen_word:
         user_life_counter -= 1
+        print(hangman_ASCII.stages[user_life_counter])
         if user_life_counter == 0: 
             print("You Lose!")
             break  
-
+### PRINT UPDATED LETTER TRAY ###
     print(f"{' '.join(display)}")
     
         
 ### CHECK IF USER WON ###    
     if "_" not in display: 
         end_of_game = True
-        print('You Win!')
+        print('\nYou Win!')
+        
