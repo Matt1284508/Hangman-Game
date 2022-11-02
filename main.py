48970b8d9f43f7755187117e88c9c4f1a8862cfd
Matt Millsaps <mattmillsaps.dev@gmail.com>
2022-11-01 21:19:27 -0400

    replace blanks with correctly guessed letters

--- main.py
+++ main.py
@@ -5,13 +5,22 @@ word_list = ["aardvark", "baboon", "camel"]
 
 ### GENERATE A RANDOM WORD ###
 chosen_word = random.choice(word_list)
+#### DEBUG ####
+print(chosen_word)
+
+### CREATE '_" PLACEHOLDERS FOR EACH LETTER IN chosen_word  ###
+display = []
+for letter in chosen_word:
+    display.append("_")
 
 ### ASK USER TO GUESS LETTER ###
 guess = input("Please Guess a letter: ").lower()
 
-### CHECK IF guess IS IN chose_word ###
-for letter in chosen_word:
+### CHECK IF guess IS IN chosen_word ###
+for position in range(len(chosen_word)):
+    letter = chosen_word[position]
     if letter == guess:
-        print('Right')
-    else:
-        print('Wrong')
+        display[position] = letter
+
+        
+print(display)
